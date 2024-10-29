10fc9b6cc368240ec1de6719019a34858982d49d
Nikhil Narayan <167934187+nikhil12322@users.noreply.github.com>
2024-10-28 20:20:26 -0700

    feat: add the resulting template

---
+++ {{ cookiecutter.solution_name }}/{{ cookiecutter.__project_cmake_name }}.cpp
@@ -0,0 +1,12 @@
+// {{ cookiecutter.__project_cmake_name }}.cpp : Defines the entry point for the application.
+//
+
+#include "{{ cookiecutter.__project_cmake_name }}.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
