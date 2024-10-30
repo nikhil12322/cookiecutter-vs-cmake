dab4b107961e96bab7b82771652e80749228f451
Nikhil Narayan <167934187+nikhil12322@users.noreply.github.com>
2024-10-29 20:35:59 -0700

    feat: now, same_dir deletes files and folders

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,12 @@
+import os
+
+REMOVE_PATHS = [
+    '{% if not cookiecutter.same_dir %}{{ cookiecutter.__project_name }}.cpp{% endif %}',
+    '{% if not cookiecutter.same_dir %}{{ cookiecutter.__project_name }}.h{% endif %}',
+    '{% if cookiecutter.same_dir %}{{ cookiecutter.__project_name }}\\{% endif %}',
+]
+
+for path in REMOVE_PATHS:
+    path = path.strip()
+    if path and os.path.exists(path):
+        os.unlink(path) if os.path.isfile(path) else os.rmdir(path)
